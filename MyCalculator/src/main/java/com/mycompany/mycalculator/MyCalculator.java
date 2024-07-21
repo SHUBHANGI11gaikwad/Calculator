321637dbc8f598f7e854c8c2c1c4fb469da3ea0c
shubhangi <114339483+SHUBHANGI11gaikwad@users.noreply.github.com>
2024-07-21 17:56:03 +0530

    Add files via upload

---
+++ MyCalculator/src/main/java/com/mycompany/mycalculator/MyCalculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mycalculator;
+
+/**
+ *
+ * @author Admin
+ */
+public class MyCalculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
